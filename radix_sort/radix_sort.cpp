d214ad0d8fdd6b27060088a81d0d4736fda5162a
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2022-06-25 04:27:44 +0300

    radix sort: base pick optimized/improved

--- radix_sort/radix_sort.cpp
+++ radix_sort/radix_sort.cpp
@@ -6,7 +6,6 @@
 #include <ranges>
 #include <utility>
 #include <algorithm>
-#include <functional>
 
 
 
@@ -22,13 +21,7 @@ static uint8_t _radix_sort_log(T max, uint8_t base_log2)
 	return result;
 }
 
-static constexpr uint8_t _radix_sort_bases_logs[] = { 5, 8, 10, 12, 14, 16, 18 };
-static constexpr uint8_t _radix_sort_max_base_log = *std::max_element(std::begin(_radix_sort_bases_logs), std::end(_radix_sort_bases_logs));
-static constexpr uint16_t _radix_sort_time_ratios[] = {1193, 5750, 16343, 25519, 25477, 26375, 25632};
-
-static_assert(std::size(_radix_sort_bases_logs) == std::size(_radix_sort_time_ratios));
-
-
+static constexpr uint8_t _radix_sort_max_base_log = 8;
 
 template<class T>
 struct _radix_sort_data_t
@@ -43,7 +36,6 @@ bool _radix_sort_data_t<T>::always_free_auxillary = true;
 
 template<class T>
 thread_local _radix_sort_data_t<T> _radix_sort_data;
-
 
 
 
@@ -126,61 +118,12 @@ void make_clean_vector(T& v, size_t n)
 	v.resize(n);
 }
 
-template<class Iter>
-auto _radix_sort_bind(uint8_t base_log2, uint8_t iterations)
+template<class T>
+std::pair<uint8_t, uint8_t> _radix_sort_get_optimal_base(const std::make_unsigned_t<T>& max, size_t n)
 {
-	struct
-	{
-		uint8_t base_log2;
-		uint8_t iterations;
-
-		void operator()(Iter begin, Iter end) const noexcept
-		{
-			radix_sort_based(begin, end, this->base_log2, this->iterations);
-		}
-	} binder{};
-
-	binder.base_log2 = base_log2;
-	binder.iterations = iterations;
-	return binder;
-}
-
-template<class Iter, class T>
-auto _radix_sort_get_based_callee(const std::make_unsigned_t<T>& max, size_t n)
-{
-	//TODO: contract: max >= 1
-	if (max < (1 << _radix_sort_max_base_log))
-	{
-		size_t base_index = 0;
-		while (base_index < std::size(_radix_sort_bases_logs))
-		{
-			if (max < (1u << _radix_sort_bases_logs[base_index]))
-				break;
-			++base_index;
-		}
-		return _radix_sort_bind<Iter>(_radix_sort_bases_logs[base_index], 1);
-	}
-
-	size_t base_index = 0;
-	size_t min_base_index;
-	int64_t min_time_estimate = INT64_MAX;
-	uint8_t min_iterations;
-	while (base_index < std::size(_radix_sort_bases_logs))
-	{
-		auto base = 1ll << _radix_sort_bases_logs[base_index];
-		uint8_t iterations = _radix_sort_log(max, _radix_sort_bases_logs[base_index]) + 1;
-
-		int64_t time_estimate = iterations * (base * 10000 + static_cast<int64_t>(n) * _radix_sort_time_ratios[base_index]);
-		if (time_estimate < min_time_estimate)
-		{
-			min_time_estimate = time_estimate;
-			min_base_index = base_index;
-			min_iterations = iterations;
-		}
-
-		++base_index;
-	}
-	return _radix_sort_bind<Iter>(_radix_sort_bases_logs[min_base_index], min_iterations);
+	uint8_t base_log2 = max < _radix_sort_max_base_log ? max : _radix_sort_max_base_log;
+	uint8_t iterations = _radix_sort_log(max, base_log2) + 1;
+	return { base_log2, iterations };
 }
 
 template<std::random_access_iterator Iter>
@@ -195,14 +138,19 @@ void radix_sort(Iter begin, Iter end) noexcept
 	make_clean_vector(_radix_sort_data<T>.auxillary, n);
 
 	auto [pmin, pmax] = std::minmax_element(begin, end);
-	auto&& min = *pmin;
+	T min = *pmin;
 	auto&& max = *pmax;
-	if (min < 0)
-		abort(); //TODO
-	
-	auto sort_func = _radix_sort_get_based_callee<Iter, T>(max, n);
 
-	std::invoke(sort_func, begin, end);
+	if (min < 0)
+		for (auto p = begin; p != end; ++p)
+			*p -= min;
+	
+	auto [base_log2, iterations] = _radix_sort_get_optimal_base<T>(max, n);
+	radix_sort_based(begin, end, base_log2, iterations);
+
+	if (min < 0)
+		for (auto p = begin; p != end; ++p)
+			*p += min;
 
 	if (_radix_sort_data<T>.always_free_auxillary)
 		reconstruct(_radix_sort_data<T>.auxillary);
@@ -216,7 +164,7 @@ void radix_sort(Iter begin, Iter end) noexcept
 #include <random>
 #include <iostream>
 
-static constexpr size_t N = 100'000'000;
+static constexpr size_t N = 10'000'000;
 using arr_t = std::array<unsigned int, N>;
 
 template<class callable_t>
@@ -282,6 +230,7 @@ int main()
 		radix_sort(x.begin(), x.end());
 	};
 
+	measure(starting_arr, test_std_sort, "std::sort");
+	measure(starting_arr, test_std_stable_sort, "std::stable_sort");
 	measure(starting_arr, test_radix_sort, "LSD Radix sort (base autopicked)");
-	//measure(starting_arr, test_std_sort, "std::sort");
 }
